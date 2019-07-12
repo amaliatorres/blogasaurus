62196338ca5c8e035e9fdd82c85a46344c0142c6
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-12 10:02:40 -0400

    new laptop

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("I'm working!!");
+
+function onHover()
+{
+    $("#pusheenImage").attr('src', 'pusheen.png');
+}
+
+function offHover()
+{
+    $("#pusheenImage").attr('src', 'pusheen.png');
+}
